b26e8a9ed3ba96b4a5d413bfb63ca0e8d28f6cb8
Your Name <goncharovigor12@gmail.com>
2022-07-02 11:04:02 +0300

    add1

---
+++ utils/checkAuth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+export default(req, res, next)=>{
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+    
+    if (token){
+        try {
+            const decoded = jwt.verify(token, 'secretkey');
+            req.userId = decoded._id;
+            next();
+        }
+        catch(e){
+            return res.status(403).json({
+                message: 'access denied'
+            })
+        }
+    }
+    else{
+        return res.status(403).json({
+            message: 'access denied'
+        })
+    }
+    
+    // res.send(token);
+    // next();
+}
