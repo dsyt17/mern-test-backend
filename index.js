12eff22cbc46997068e1edec0f21e45a4c727c30
Your Name <goncharovigor12@gmail.com>
2022-08-03 23:32:58 +0300

    fs23

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
         if (!fs.existsSync('uploads')){
-            fs.mkdirSynk('uploads');
+            fs.mkdirSync('uploads');
         };
         cb(null, 'uploads');
     },
